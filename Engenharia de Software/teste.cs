f5638f67fcf544fe1958c4e88be76b3eb9a598fd
Guilherme Lefrancois <gui.lefrancois21@gmail.com>
2020-06-06 16:27:19 -0300

    Teste_numero 01

---
+++ Engenharia de Software/teste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engenharia_de_Software
+{
+    public class teste
+    {
+        private string nome;
+        private string email;
+        private string endereco;
+
+        public teste()
+        { 
+            
+        }
+    }
+}
